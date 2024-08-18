c142e5077b91630cb50751287322a2426a1bf4f4
juanpythonvolei <juanzsalca@outlook.com>
2024-08-18 15:53:34 -0300

    Update Consulta.py

--- Estoque/pages/Consulta.py
+++ Estoque/pages/Consulta.py
@@ -38,7 +38,7 @@ if produtos:
                         lista_foto.append(foto)
                         
                       localizacao = espec['localicação']
-                      texto += f'Produto {codigo} possui: {quantidade} unidades na posição {localizacao} do depósito: Revenda'\n
+                      texto += f'Produto {codigo} possui: {quantidade} unidades na posição {localizacao} do depósito: Revenda  \n'
   with descricao_visual:
       foto = st.image(lista_foto[0])
       st.info(texto)
