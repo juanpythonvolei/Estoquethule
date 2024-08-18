999b52a7d29ea02fcd72d23aab9eb2f1bbd35c91
juanpythonvolei <juanzsalca@outlook.com>
2024-08-18 15:12:46 -0300

    Update Consulta.py

--- Estoque/pages/Consulta.py
+++ Estoque/pages/Consulta.py
@@ -4,4 +4,14 @@ from firebase_admin import credentials, firestore,db
 import requests
 import os
 import requests
-
+st.image()
+requiscao = requests.get('https://bancodedadosroteirooficial-default-rtdb.firebaseio.com/.json')
+roteiro = requiscao.json()
+dados = roteiro['Produtos']
+lista_nomes = []
+for item in dados:
+                item_estoque = dados[f'{item}']
+                for elemento in item_estoque:
+                    espec = item_estoque[f'{elemento}']
+                    codigo = espec['Código']
+                    lista_nomes.append(codigo)
