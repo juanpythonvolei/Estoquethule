b5fb4feaf72a80ce9f3d7f88d4778a11f5326077
juanpythonvolei <juanzsalca@outlook.com>
2024-08-18 15:14:17 -0300

    Update Consulta.py

--- Estoque/pages/Consulta.py
+++ Estoque/pages/Consulta.py
@@ -4,14 +4,15 @@ from firebase_admin import credentials, firestore,db
 import requests
 import os
 import requests
-st.image()
+image = st.image('https://www.logolynx.com/images/logolynx/fe/fe346f78d111e1d702b44186af59b568.jpeg')
 requiscao = requests.get('https://bancodedadosroteirooficial-default-rtdb.firebaseio.com/.json')
 roteiro = requiscao.json()
 dados = roteiro['Produtos']
-lista_nomes = []
+lista_produtos = []
 for item in dados:
                 item_estoque = dados[f'{item}']
                 for elemento in item_estoque:
                     espec = item_estoque[f'{elemento}']
                     codigo = espec['Código']
-                    lista_nomes.append(codigo)
+                    lista_produtos.append(codigo)
+produtos = st.selectbox(label='',placeholder='Selecione o Produto',lista_produtos)
