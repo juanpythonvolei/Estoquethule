08f945d13933de95cdf0ce225d10b2d546b56b09
juanpythonvolei <juanzsalca@outlook.com>
2024-08-18 13:42:06 -0300

    Update Cadastro.py

--- Estoque/pages/Cadastro.py
+++ Estoque/pages/Cadastro.py
@@ -25,7 +25,7 @@ def exclusao():
                 lista_nomes.append(nome)
     produto_excluir = st.selectbox(label='Selecione o produto',options=lista_nomes,index=None)
     if produto_excluir:
-        veiculo_ref = db.reference(f'Estoque/Produtos/{produto_excluir}')
+        veiculo_ref = db.reference(f'Estoque/Produtos/{item_estoque}/{produto_excluir}')
         veiculo_ref.delete()
         st.success(f'Produto {produto_excluir} excluido')
     else:
