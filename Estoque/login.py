ab74a820ff39eba5de439b2996346fb0bb8ec2a7
juanpythonvolei <juanzsalca@outlook.com>
2024-08-18 13:15:53 -0300

    Add files via upload

---
+++ Estoque/login.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import firebase_admin
+from firebase_admin import credentials, firestore,db
+import requests
+import os
+
+
+if not firebase_admin._apps:
+    autenticacao  = {
+            "type": st.secrets["firebase"]["type"],
+            "project_id": st.secrets["firebase"]["project_id"],
+            "private_key_id": st.secrets["firebase"]["private_key_id"],
+            "private_key": st.secrets["firebase"]["private_key"],
+            "client_email": st.secrets["firebase"]["client_email"],
+            "client_id": st.secrets["firebase"]["client_id"],
+            "auth_uri": st.secrets["firebase"]["auth_uri"],
+            "token_uri": st.secrets["firebase"]["token_uri"],
+            "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+            "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"]
+    }
+
+    cred = credentials.Certificate(autenticacao)
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://bancodedadosroteirooficial-default-rtdb.firebaseio.com'  # Substitua pelo URL do seu Realtime Database
+    })
+else:
+    pass
+
---
+++ Estoque/pages/Cadastro.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import firebase_admin
+from firebase_admin import credentials, firestore,db
+import requests
+import os
+import requests
+image = st.image('https://www.logolynx.com/images/logolynx/fe/fe346f78d111e1d702b44186af59b568.jpeg')
+codigo = st.text_input(label='',placeholder='Insira o Código do produto')
+descricao = st.text_input(label='',placeholder='Insira uma descirção do produto')
+col1,col2,col3 = st.columns(3)
+
+
+@st.dialog(f"Deseja realmente excluir o produtos") 
+def exclusao(excluir):                                                             
+    produto_excluir = st.select_slider()
+    if produto_excluir:
+        veiculo_ref = db.reference(f'Estoque/Produtos/{produto_excluir}')
+        veiculo_ref.delete()
+        st.success(f'Veículo {produto_excluir} excluido')
+    else:
+        st.info(f'Você realmente deseja excluir o produto {produto_excluir}')
+
+
+with col1:
+    localizacao = st.text_input(label='',placeholder='Insira a localização')
+    
+with col2:  
+    quantidade = st.number_input(label='',placeholder='Insira a quantidade')
+with col3:
+    foto = st.text_input(label='',placeholder='Insira a foto do Produto')
+    col=localizacao[3:6]
+    Prat=localizacao[0:2]
+    alt = localizacao[7:]
+    if len(col) != 3 or len(alt)!= 2 or len(Prat)!=2:
+        st.error(f'A posição {localizacao} está incorreta. Insira-a novamente')
+        localizacao = ''
+    excluir = st.button('Excluir Produtos')
+    if excluir:
+        exclusao()
+ref = db.reference('Estoque')
+if codigo and descricao and quantidade and foto and localizacao:
+    with col1:
+        cadastro = st.button('Cadastrar')
+        if cadastro:
+            dict_produto = {'Código':codigo,'Quantidade':quantidade,'Foto':foto,'Descrição':descricao}
+            try:
+                ref.child('Produtos').push().set(dict_produto)
+                st.success(f'Protudo de código: {codigo} cadastrado com sucesso')
+            except:
+                st.error('Não há saida de dados disponível')
