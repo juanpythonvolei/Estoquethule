ca386122d30725e70ae3c49ff48dc8c317741788
juanpythonvolei <juanzsalca@outlook.com>
2024-08-18 21:27:11 -0300

    Update Tranferência.py

--- Estoque/pages/Tranferência.py
+++ Estoque/pages/Tranferência.py
@@ -4,4 +4,14 @@ from firebase_admin import credentials, firestore,db
 import requests
 import os
 import requests
+col1,col2,col3 = st.columns(3)
+image = st.image('https://www.logolynx.com/images/logolynx/fe/fe346f78d111e1d702b44186af59b568.jpeg')
+with col1:
+  deposito_origem = st.selecbox(index=None,label='',placeholder='Selecione o depósito de origem',options=['Rev','Dev','Rec','Ele'])
+with col2:
+  deposito_final= st.selecbox(index=None,label='',placeholder='Selecione o depósito de origem',options=['Rev'])
 
+Origem = st.text_input(label='',palceholder='Insira a posição de Origem')
+produto = st.text_input(label='',placeholder='Insira o produto')
+produto = st.text_input(label='',placeholder='Insira a quantidade')
+produto = st.text_input(label='',placeholder='Insira a posição Final')
