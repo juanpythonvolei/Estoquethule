a92776b99a61eb6fa208b4378ab1ec5d0ee4889f
juanpythonvolei <juanzsalca@outlook.com>
2024-08-18 22:23:51 -0300

    Update Tranferência.py

--- Estoque/pages/Tranferência.py
+++ Estoque/pages/Tranferência.py
@@ -14,10 +14,10 @@ if deposito_origem == 'Rec' or deposito_origem == 'Dev'or deposito_origem == 'El
     origem = st.text_input(label='',placeholder='Insira a posição de Origem',value=deposito_origem)
 else:
     origem = st.text_input(label='',placeholder='Insira a posição de Origem')
-    col2=origem [3:6]
+    colum2=origem [3:6]
     Prat2=origem [0:2]
     alt2 = origem [7:]
-    if len(col2) != 3 or len(alt2)!= 2 or len(Prat2)!=2:
+    if len(colum2) != 3 or len(alt2)!= 2 or len(Prat2)!=2:
         st.error(f'A posição {origem} está incorreta. Insira-a novamente')
         localizacao = ''
 with col2:
@@ -33,10 +33,10 @@ if deposito_origem and deposito_final:
     st.error('Atenção. Ainda há campos a serem preenchidos')
   else:
     st.button(f'Transferir {produto}')
-    col=final[3:6]
+    colum=final[3:6]
     Prat=final[0:2]
     alt = final[7:]
-    if len(col) != 3 or len(alt)!= 2 or len(Prat)!=2:
+    if len(colum) != 3 or len(alt)!= 2 or len(Prat)!=2:
         st.error(f'A posição {final} está incorreta. Insira-a novamente')
         localizacao = ''
   
