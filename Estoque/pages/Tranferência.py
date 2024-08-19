3502e92af9e0c6dca2151757d9e4d2e2c4a8f8d8
juanpythonvolei <juanzsalca@outlook.com>
2024-08-18 21:27:57 -0300

    Update Tranferência.py

--- Estoque/pages/Tranferência.py
+++ Estoque/pages/Tranferência.py
@@ -7,9 +7,9 @@ import requests
 col1,col2,col3 = st.columns(3)
 image = st.image('https://www.logolynx.com/images/logolynx/fe/fe346f78d111e1d702b44186af59b568.jpeg')
 with col1:
-  deposito_origem = st.selecbox(index=None,label='',placeholder='Selecione o depósito de origem',options=['Rev','Dev','Rec','Ele'])
+  deposito_origem = st.selectbox(index=None,label='',placeholder='Selecione o depósito de origem',options=['Rev','Dev','Rec','Ele'])
 with col2:
-  deposito_final= st.selecbox(index=None,label='',placeholder='Selecione o depósito de origem',options=['Rev'])
+  deposito_final= st.selectbox(index=None,label='',placeholder='Selecione o depósito de origem',options=['Rev'])
 if deposito_origem and deposito_final:
   Origem = st.text_input(label='',palceholder='Insira a posição de Origem')
   produto = st.text_input(label='',placeholder='Insira o produto')
