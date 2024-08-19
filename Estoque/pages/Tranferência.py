d4518c70361f1e1b4170ed1ff638010cfc04b1ce
juanpythonvolei <juanzsalca@outlook.com>
2024-08-18 22:07:28 -0300

    Update Tranferência.py

--- Estoque/pages/Tranferência.py
+++ Estoque/pages/Tranferência.py
@@ -10,12 +10,14 @@ col1,col2,col3 = st.columns(3)
 
 with col1:
   deposito_origem = st.selectbox(index=None,label='',placeholder='depósito de origem',options=['Rev','Dev','Rec','Ele'])
-  if deposito_origem == 'Rec' or deposito_origem == 'Dev'or deposito_origem == 'Ele':
+if deposito_origem == 'Rec' or deposito_origem == 'Dev'or deposito_origem == 'Ele':
     Origem = st.text_input(label='',placeholder='Insira a posição de Origem',value=deposito_origem)
 with col2:
   deposito_final= st.selectbox(index=None,label='',placeholder='depósito de origem',options=['Rev'])
 if deposito_origem and deposito_final:
   Origem = st.text_input(label='',placeholder='Insira a posição de Origem')
   produto = st.text_input(label='',placeholder='Insira o produto')
-  produto = st.text_input(label='',placeholder='Insira a quantidade')
-  produto = st.text_input(label='',placeholder='Insira a posição Final')
+  quantidade = st.text_input(label='',placeholder='Insira a quantidade')
+  final = st.text_input(label='',placeholder='Insira a posição Final')
+  if Produto:
+    st.info(produto)
