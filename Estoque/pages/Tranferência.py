b9df72575c17aa90cbe5b0f81e4d34d51fbb8d36
juanpythonvolei <juanzsalca@outlook.com>
2024-08-18 22:33:09 -0300

    Update Tranferência.py

--- Estoque/pages/Tranferência.py
+++ Estoque/pages/Tranferência.py
@@ -7,12 +7,14 @@ import requests
 
 image = st.image('https://www.logolynx.com/images/logolynx/fe/fe346f78d111e1d702b44186af59b568.jpeg')
 col1,col2,col3 = st.columns(3)
-
 with col1:
   deposito_origem = st.selectbox(index=None,label='',placeholder='depósito de origem',options=['Rev','Dev','Rec','Ele'])
-if deposito_origem == 'Rec' or deposito_origem == 'Dev'or deposito_origem == 'Ele':
+with col2:
+  deposito_final= st.selectbox(index=None,label='',placeholder='depósito de origem',options=['Rev'])
+if deposito_origem and deposito_final:
+  if deposito_origem == 'Rec' or deposito_origem == 'Dev'or deposito_origem == 'Ele':
     origem = st.text_input(label='',placeholder='Insira a posição de Origem',value=deposito_origem)
-else:
+  else:
     origem = st.text_input(label='',placeholder='Insira a posição de Origem')
     colum2=origem [3:6]
     Prat2=origem [0:2]
@@ -20,10 +22,6 @@ else:
     if len(colum2) != 3 or len(alt2)!= 2 or len(Prat2)!=2:
         st.error(f'A posição {origem} está incorreta. Insira-a novamente')
         localizacao = ''
-with col2:
-  deposito_final= st.selectbox(index=None,label='',placeholder='depósito de origem',options=['Rev'])
-if deposito_origem and deposito_final:
-
   produto = st.text_input(label='',placeholder='Insira o produto')
   quantidade = st.text_input(label='',placeholder='Insira a quantidade')
   final = st.text_input(label='',placeholder='Insira a posição Final')
