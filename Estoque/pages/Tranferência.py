9a00999c5161ab10ef4cd44e0dee40c594be2d67
juanpythonvolei <juanzsalca@outlook.com>
2024-08-18 21:28:41 -0300

    Update Tranferência.py

--- Estoque/pages/Tranferência.py
+++ Estoque/pages/Tranferência.py
@@ -4,12 +4,14 @@ from firebase_admin import credentials, firestore,db
 import requests
 import os
 import requests
-col1,col2,col3 = st.columns(3)
+
 image = st.image('https://www.logolynx.com/images/logolynx/fe/fe346f78d111e1d702b44186af59b568.jpeg')
+col1,col2,col3 = st.columns(3)
+
 with col1:
-  deposito_origem = st.selectbox(index=None,label='',placeholder='Selecione o depósito de origem',options=['Rev','Dev','Rec','Ele'])
+  deposito_origem = st.selectbox(index=None,label='',placeholder='depósito de origem',options=['Rev','Dev','Rec','Ele'])
 with col2:
-  deposito_final= st.selectbox(index=None,label='',placeholder='Selecione o depósito de origem',options=['Rev'])
+  deposito_final= st.selectbox(index=None,label='',placeholder='depósito de origem',options=['Rev'])
 if deposito_origem and deposito_final:
   Origem = st.text_input(label='',palceholder='Insira a posição de Origem')
   produto = st.text_input(label='',placeholder='Insira o produto')
