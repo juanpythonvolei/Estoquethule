9efe3d8f9daacd06db0fc3a8b3e27c2c3ad0b847
juanpythonvolei <juanzsalca@outlook.com>
2024-08-20 23:24:25 -0300

    Update Recebimento.py

--- Estoque/pages/Recebimento.py
+++ Estoque/pages/Recebimento.py
@@ -6,7 +6,7 @@ import os
 import requests
 
 image = st.image('https://www.logolynx.com/images/logolynx/fe/fe346f78d111e1d702b44186af59b568.jpeg')
-
+st.write('Está funcionando')
 tab1,tab2 = st.tabs(['Alimentar Rec (Manual)','Iten em nota '])
 with tab1:
   produto = st.selectbox(label='',placeholder='Selecione um Produto',['Opção'])
