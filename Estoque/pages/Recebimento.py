68b635b544a4d3e53e230bb8aa5c648152be2296
juanpythonvolei <juanzsalca@outlook.com>
2024-08-20 23:39:09 -0300

    Update Recebimento.py

--- Estoque/pages/Recebimento.py
+++ Estoque/pages/Recebimento.py
@@ -1,1 +1,8 @@
 
+import streamlit as st
+import firebase_admin
+from firebase_admin import credentials, firestore,db
+import requests
+import os
+import requests
+image = st.image('https://www.logolynx.com/images/logolynx/fe/fe346f78d111e1d702b44186af59b568.jpeg')
