e829eabf42877903bc62266b58e07247f34ce3a8
juanpythonvolei <juanzsalca@outlook.com>
2024-08-20 23:21:03 -0300

    Update Recebimento.py

--- Estoque/pages/Recebimento.py
+++ Estoque/pages/Recebimento.py
@@ -1,1 +1,12 @@
+import streamlit as st
+import firebase_admin
+from firebase_admin import credentials, firestore,db
+import requests
+import os
+import requests
 
+image = st.image('https://www.logolynx.com/images/logolynx/fe/fe346f78d111e1d702b44186af59b568.jpeg')
+
+tab1,tab2 = st.tabs(['Alimentar Rec (Manual)','Iten em nota '])
+with tab1:
+  Produto = st.selectbox(label='',placeholder='Selecione um Produto',['Opção'])
