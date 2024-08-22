b0fc0dc35bb1a3f17c8f53c1343c0155eaa4bae5
juanpythonvolei <juanzsalca@outlook.com>
2024-08-21 22:23:45 -0300

    Update Recebimento.py

--- Estoque/pages/Recebimento.py
+++ Estoque/pages/Recebimento.py
@@ -6,9 +6,22 @@ import requests
 import os
 import requests
 image = st.image('https://www.logolynx.com/images/logolynx/fe/fe346f78d111e1d702b44186af59b568.jpeg')
+requiscao = requests.get('https://bancodedadosroteirooficial-default-rtdb.firebaseio.com/.json')
+roteiro = requiscao.json()
+dados = roteiro['Estoque']
+lista_produtos = []
+for item in dados:
+                item_estoque = dados[f'{item}']
+                for elemento in item_estoque:
+                    espec = item_estoque[f'{elemento}']
+                    codigo = espec['Código']
+                    if codigo in lista_produtos:
+                      pass
+                    else:
+                      lista_produtos.append(codigo)
 tab1,tab2 = st.tabs(['Alimentação Manual','Alimentação de itens da nota'])
 with tab1:
-  item = st.selectbox(label='',placeholder='Insira um item',options=['vazio'])
+  item = st.selectbox(label='',placeholder='Insira um item',options=lista_produtos)
   quantidade = st.number_input(placeholder=f'Insira a quantidade do item: {item}',value=None,label='')
   if item and quantidade:
     st.button(f'Adicionar Item: {item}')
