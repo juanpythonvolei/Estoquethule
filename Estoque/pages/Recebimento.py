64d21963e1b6166bd497e5272736baf884d32a26
juanpythonvolei <juanzsalca@outlook.com>
2024-08-21 22:18:48 -0300

    Update Recebimento.py

--- Estoque/pages/Recebimento.py
+++ Estoque/pages/Recebimento.py
@@ -7,3 +7,9 @@ import os
 import requests
 image = st.image('https://www.logolynx.com/images/logolynx/fe/fe346f78d111e1d702b44186af59b568.jpeg')
 tab1,tab2 = st.tabs(['Alimentação Manual','Alimentação de itens da nota'])
+with tab1:
+  item = st.selectbox(label='',palceholder='Insira um item',['vazio'])
+  quantidade = st.number_input(placeholder=f'Insira a quantidade do item: {item}',value=None,label='')
+  if item and quantidade:
+    st.button(f'Adicionar Item: {item}')
+  
