bd04b81b2be2f6b9f37c3340d1a054be378a6330
juanpythonvolei <juanzsalca@outlook.com>
2024-08-22 23:31:06 -0300

    Update Tranferência.py

--- Estoque/pages/Tranferência.py
+++ Estoque/pages/Tranferência.py
@@ -5,7 +5,6 @@ import requests
 import os
 import requests           
 def consulta(item):    
-  
   requiscao = requests.get('https://bancodedadosroteirooficial-default-rtdb.firebaseio.com/.json')
   roteiro = requiscao.json()
   dados = roteiro['Depósito']
@@ -30,7 +29,7 @@ col1,col2,col3 = st.columns(3)
 with col1:
   deposito_origem = st.selectbox(index=None,label='',placeholder='Depósito de origem',options=['Rev','Dev','Rec','Ele'])
 with col2:
-  deposito_final= st.selectbox(index=None,label='',placeholder='Depósito de origem',options=['Rev','Ele'])
+  deposito_final= st.selectbox(index=None,label='',placeholder='Depósito final',options=['Rev','Ele'])
 if deposito_origem and deposito_final:
   if deposito_origem == 'Rec' or deposito_origem == 'Dev'or deposito_origem == 'Ele':
     origem = st.text_input(label='Insira a posição de Origem',value=deposito_origem)
