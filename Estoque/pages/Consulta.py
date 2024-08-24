1d39632434f2a1ef604c79eb4007658e612c8f94
juanpythonvolei <juanzsalca@outlook.com>
2024-08-24 20:33:37 -0300

    Update Consulta.py

--- Estoque/pages/Consulta.py
+++ Estoque/pages/Consulta.py
@@ -8,7 +8,14 @@ import time
 import google.generativeai as genai
 import pandas as pd
 
-
+GOOGLE_API_KEY = st.secrets['firebase']['GOOGLE_API_KEY']
+genai.configure(api_key=GOOGLE_API_KEY)
+model = genai.GenerativeModel('gemini-1.5-flash')
+chat = model.start_chat(history=[])
+def consulta_itens_e_posicoes(tema):
+     response = chat.send_message(f'Você receberá a seguir um conjunto de dados relacionados a um estoque. Por favor responda o que for possível conforme o solicitado. Segue a pergunta:{comando}\n\n{texto_problemas}\n')
+     resposta = response.text
+     st.info(resposta) 
 image = st.image('https://www.logolynx.com/images/logolynx/fe/fe346f78d111e1d702b44186af59b568.jpeg')
 requiscao = requests.get('https://bancodedadosroteirooficial-default-rtdb.firebaseio.com/.json')
 roteiro = requiscao.json()
