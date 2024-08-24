3625f20ac94896a3710d446344b38f798e01fd59
juanpythonvolei <juanzsalca@outlook.com>
2024-08-24 20:44:07 -0300

    Update Consulta.py

--- Estoque/pages/Consulta.py
+++ Estoque/pages/Consulta.py
@@ -68,4 +68,4 @@ elif menu == "consultar posição":
                                 '''
 )
 elif menu == 'Assistente':
-     pergunta = st.chatmessage('human')
+     pergunta = st.chat_message('human')
