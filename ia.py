4a954c735ab18e92392083fd27cce4b6f686f48b
juanpythonvolei <juanzsalca@outlook.com>
2024-08-24 19:40:13 -0300

    Update ia.py

--- ia.py
+++ ia.py
@@ -1,3 +1,12 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+import requests
+import time
+import google.generativeai as genai
+import pandas as pd
+from Adicionar_Imagens import exibir_imagem
+
+
 GOOGLE_API_KEY = st.secrets['firebase']['GOOGLE_API_KEY']
 genai.configure(api_key=GOOGLE_API_KEY)
 model = genai.GenerativeModel('gemini-1.5-flash')
