ca7a5bf4570c69dd8802263a84d6c555ca9416a0
juanpythonvolei <juanzsalca@outlook.com>
2024-08-24 18:46:12 -0300

    Update Consulta.py

--- Estoque/pages/Consulta.py
+++ Estoque/pages/Consulta.py
@@ -8,11 +8,14 @@ from streamlit_option_menu import option_menu
 image = st.image('https://www.logolynx.com/images/logolynx/fe/fe346f78d111e1d702b44186af59b568.jpeg')
 requiscao = requests.get('https://bancodedadosroteirooficial-default-rtdb.firebaseio.com/.json')
 roteiro = requiscao.json()
+dados3 = roteiro['Depósito']['Rec']
 dados = roteiro['Depósito']['Rev']
 dados2 = roteiro['Estoque']
 quantidade = 0
 menu = option_menu('selecione uma opção',['Consultar item','consultar posição'])
 if menu == 'Consultar item':
+  item_rec = dados3[f'{selecao_item}']['quantidade']
+  st.warning(f'O item {selecao_item} possúi {item_rec} unidades em Rec')
   texto_item = ''
   lista_itens = [elemento for elemento in dados2]
   selecao_item = st.selectbox(label = '',placeholder='Selecione um Item',options=lista_itens,index=None)
