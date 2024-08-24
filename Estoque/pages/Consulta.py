1cd758982fd3c334199e14ce955efe13d7de668b
juanpythonvolei <juanzsalca@outlook.com>
2024-08-24 18:23:31 -0300

    Update Consulta.py

--- Estoque/pages/Consulta.py
+++ Estoque/pages/Consulta.py
@@ -4,11 +4,14 @@ from firebase_admin import credentials, firestore,db
 import requests
 import os
 import requests
+from streamlit_option_menu import option_menu
 image = st.image('https://www.logolynx.com/images/logolynx/fe/fe346f78d111e1d702b44186af59b568.jpeg')
 requiscao = requests.get('https://bancodedadosroteirooficial-default-rtdb.firebaseio.com/.json')
 roteiro = requiscao.json()
 dados = roteiro['Depósito']
 dados2 = roteiro['Estoque']
 lista_itens = [elemento for elemento in dados2]
-selecao = st.selectbox(label = '',placeholder='Selecione um Item',options=lista_itens)
+selecao = st.selectbox(label = '',placeholder='Selecione um Item',options=lista_itens,index=None)
+quantidade = 0
+menu = option_menu('selecione uma opção',['Consultar item','consultar posição'])
 
