ee3a690bbc75c09a267d192a96a9a91e0f3bfec8
juanpythonvolei <juanzsalca@outlook.com>
2024-08-27 22:48:23 -0300

    Update Consulta.py

--- Estoque/pages/Consulta.py
+++ Estoque/pages/Consulta.py
@@ -12,80 +12,55 @@ GOOGLE_API_KEY = st.secrets['firebase']['GOOGLE_API_KEY']
 genai.configure(api_key=GOOGLE_API_KEY)
 model = genai.GenerativeModel('gemini-1.5-flash')
 chat = model.start_chat(history=[])
-def consulta_itens_e_posicoes(a,b):
-     response = chat.send_message(f'Você receberá a seguir um conjunto de dados relacionados a um estoque. Por favor responda o que for possível conforme o solicitado. Segue a pergunta:{a}\n\n{b}\n')
-     resposta = response.text
-     st.info(resposta) 
+
 image = st.image('https://www.logolynx.com/images/logolynx/fe/fe346f78d111e1d702b44186af59b568.jpeg')
 requiscao = requests.get('https://bancodedadosroteirooficial-default-rtdb.firebaseio.com/.json')
 roteiro = requiscao.json()
-dados3 = roteiro['Depósito']['Rec']
-dados = roteiro['Depósito']['Rev']
-dados2 = roteiro['Estoque']
-quantidade = 0
-col1,col2,col3 = st.tabs(['Consultar item','consultar posição','Assistente'])
-with col1:
- 
-  texto_item = ''
-  lista_itens = [elemento for elemento in dados2]
-  coletor = st.toggle('Coletor',key='posicao') 
-  if coletor:
-       selecao_item = st.text_input(label = '',placeholder='Insira o item')
-  else:   
-       selecao_item = st.selectbox(label = '',placeholder='Selecione um Item',options=lista_itens,index=None)   
-  if selecao_item:   
-    item_rec = dados3[f'{selecao_item}']['quantidade']
-    foto = dados2[f'{selecao_item}'][f'foto']
-    st.warning(f'O item {selecao_item} possúi {item_rec} unidades em Rec')
-    for item in dados:
-      posicao = dados[f'{item}']
-      for produto in posicao:
-        if produto == selecao_item:
-           quantidade = posicao[f'{produto}']['quantidade']
-           info =  f'''
-           O item {selecao_item} possúi {quantidade} unidades na posição {item}
-           '''
-           texto_item += info
-    st.info(texto_item) 
-    st.image(foto)       
-with col2:
-  texto_posicao =''
-  lista_posicoes = [elemento for elemento in dados]
-  coletor = st.toggle('Coletor',key='localizacao')    
-  if coletor:
-       selecao_posicao = st.text_input(label = '',placeholder='Selecione uma posição')
-  else:   
-       selecao_posicao = st.selectbox(label = '',placeholder='Selecione uma posição',options=lista_posicoes,index=None)
-  if selecao_posicao:
-   for item in dados:
-     if item == selecao_posicao:
-      posicao = dados[f'{item}']
-      for produto in posicao:
-        ativo = produto
-        quantidade = posicao[f'{produto}']['quantidade']
-        informacao = f'''
-        {ativo}-{quantidade} unidades
-        
-        '''
-        texto_posicao += informacao
-   st.info( f'''
-   A posição {selecao_posicao} possúi os segunites itens
-              
-             {texto_posicao}
-                                '''
-)
-with col3:
-    texto_item = ''        
-    for item in dados:
-      posicao = dados[f'{item}']
-      for produto in posicao:
-           quantidade = posicao[f'{produto}']['quantidade']
-           info =  f'''
-           O item {produto} possúi {quantidade} unidades na posição {item}
-           '''
-           texto_item += info
-    texto_posicao =''      
-    for item in dados:         
+if 'Depósito' in roteiro:
+     def consulta_itens_e_posicoes(a,b):
+          response = chat.send_message(f'Você receberá a seguir um conjunto de dados relacionados a um estoque. Por favor responda o que for possível conforme o solicitado. Segue a pergunta:{a}\n\n{b}\n')
+          resposta = response.text
+          st.info(resposta) 
+     dados3 = roteiro['Depósito']['Rec']
+     dados = roteiro['Depósito']['Rev']
+     dados2 = roteiro['Estoque']
+     quantidade = 0
+     col1,col2,col3 = st.tabs(['Consultar item','consultar posição','Assistente'])
+     with col1:
+      
+       texto_item = ''
+       lista_itens = [elemento for elemento in dados2]
+       coletor = st.toggle('Coletor',key='posicao') 
+       if coletor:
+            selecao_item = st.text_input(label = '',placeholder='Insira o item')
+       else:   
+            selecao_item = st.selectbox(label = '',placeholder='Selecione um Item',options=lista_itens,index=None)   
+       if selecao_item:   
+         item_rec = dados3[f'{selecao_item}']['quantidade']
+         foto = dados2[f'{selecao_item}'][f'foto']
+         st.warning(f'O item {selecao_item} possúi {item_rec} unidades em Rec')
+         for item in dados:
+           posicao = dados[f'{item}']
+           for produto in posicao:
+             if produto == selecao_item:
+                quantidade = posicao[f'{produto}']['quantidade']
+                info =  f'''
+                O item {selecao_item} possúi {quantidade} unidades na posição {item}
+                '''
+                texto_item += info
+         st.info(texto_item) 
+         st.image(foto)       
+     with col2:
+       texto_posicao =''
+       lista_posicoes = [elemento for elemento in dados]
+       coletor = st.toggle('Coletor',key='localizacao')    
+       if coletor:
+            selecao_posicao = st.text_input(label = '',placeholder='Selecione uma posição')
+       else:   
+            selecao_posicao = st.selectbox(label = '',placeholder='Selecione uma posição',options=lista_posicoes,index=None)
+       if selecao_posicao:
+        for item in dados:
+          if item == selecao_posicao:
            posicao = dados[f'{item}']
            for produto in posicao:
              ativo = produto
@@ -94,12 +69,39 @@ with col3:
              {ativo}-{quantidade} unidades
              
              '''
-             texto_posicao += informacao  
-           texto_base = f'''
-             {texto_item}
-          
-             {texto_posicao}
-        '''              
-    pergunta = st.chat_input()       
-    if pergunta:         
-                  consulta_itens_e_posicoes(pergunta,texto_base)          
+             texto_posicao += informacao
+        st.info( f'''
+        A posição {selecao_posicao} possúi os segunites itens
+                   
+                  {texto_posicao}
+                                     '''
+     )
+     with col3:
+         texto_item = ''        
+         for item in dados:
+           posicao = dados[f'{item}']
+           for produto in posicao:
+                quantidade = posicao[f'{produto}']['quantidade']
+                info =  f'''
+                O item {produto} possúi {quantidade} unidades na posição {item}
+                '''
+                texto_item += info
+         texto_posicao =''      
+         for item in dados:         
+                posicao = dados[f'{item}']
+                for produto in posicao:
+                  ativo = produto
+                  quantidade = posicao[f'{produto}']['quantidade']
+                  informacao = f'''
+                  {ativo}-{quantidade} unidades
+                  
+                  '''
+                  texto_posicao += informacao  
+                texto_base = f'''
+                  {texto_item}
+               
+                  {texto_posicao}
+             '''              
+         pergunta = st.chat_input()       
+         if pergunta:         
+                       consulta_itens_e_posicoes(pergunta,texto_base)          
