4404d1001970fdf4087dc1ae2ae54418e606064f
juanpythonvolei <juanzsalca@outlook.com>
2024-08-27 21:49:22 -0300

    Update Tranferência.py

--- Estoque/pages/Tranferência.py
+++ Estoque/pages/Tranferência.py
@@ -40,107 +40,102 @@ with col4:
     elementos = [x for x in dados2]
     if coletor:
       produto = st.text_input(label='',placeholder='Insira o produto')
-      if produto in elementos:
-        if produto not in elementos:
-          st.error('Insira um Produto cadastrado')
-        else:
-          pass
-      else:
+    else:
         produto = st.selectbox(label='',placeholder='Insira o produto',options=elementos,index=None)
-  if produto:
-    col1,col2,col3 = st.columns(3)
-    with col5: 
-              with st.popover('🔍'):
-                             consulta(produto)
-    with col1:
-          deposito_origem = st.selectbox(index=None,label='',placeholder='Depósito de origem',options=['Rev','Dev','Rec','Ele'])
-    with col2:
-          deposito_final= st.selectbox(index=None,label='',placeholder='Depósito final',options=['Rev','Ele'])
-    if deposito_origem and deposito_final:
-      if produto in elementos:
-          if deposito_origem == 'Rec' or deposito_origem == 'Dev'or deposito_origem == 'Ele':
-            origem = st.text_input(label='Insira a posição de Origem',value=deposito_origem)
-          else:
-            deposito_ref = db.reference('Depósito')
-            requiscao = requests.get('https://bancodedadosroteirooficial-default-rtdb.firebaseio.com/.json')
-            roteiro = requiscao.json()
-            dados = roteiro['Depósito']['Rev']
-            lista_pos = []
-            for item in dados:
-              posicao = dados[f'{item}']
-              for elemento in posicao:
-                              if elemento == produto:
-                                if item in lista_pos:
-                                  pass
-                                else:
-                                  lista_pos.append(item)
-            if coletor:
-              origem = st.text_input(label='',placeholder='Insira a posição de Origem')
-              
+    if produto not in elementos:
+      col1,col2,col3 = st.columns(3)
+      with col5: 
+                with st.popover('🔍'):
+                               consulta(produto)
+      with col1:
+            deposito_origem = st.selectbox(index=None,label='',placeholder='Depósito de origem',options=['Rev','Dev','Rec','Ele'])
+      with col2:
+            deposito_final= st.selectbox(index=None,label='',placeholder='Depósito final',options=['Rev','Ele'])
+      if deposito_origem and deposito_final:
+        if produto in elementos:
+            if deposito_origem == 'Rec' or deposito_origem == 'Dev'or deposito_origem == 'Ele':
+              origem = st.text_input(label='Insira a posição de Origem',value=deposito_origem)
             else:
-              origem = st.selectbox(label='',placeholder='Insira a posição de Origem',options=lista_pos,index=None)
-            if origem:
-              colum2=origem [3:6]
-              Prat2=origem [0:2]
-              alt2 = origem [7:]
-              if len(colum2) != 3 or len(alt2)!= 2 or len(Prat2)!=2:
-                  st.error(f'A posição {origem} está incorreta. Insira-a novamente')
-                  localizacao = None
-          quantidade = st.number_input(label='',placeholder='Insira a quantidade',value=None)
-          final = st.text_input(label='',placeholder='Insira a posição Final')
-          if final:
-              colum=final[3:6]
-              Prat=final[0:2]
-              alt = final[7:]
-              if len(colum) != 3 or len(alt)!= 2 or len(Prat)!=2:
-                    st.error(f'A posição {final} está incorreta. Insira-a novamente')
+              deposito_ref = db.reference('Depósito')
+              requiscao = requests.get('https://bancodedadosroteirooficial-default-rtdb.firebaseio.com/.json')
+              roteiro = requiscao.json()
+              dados = roteiro['Depósito']['Rev']
+              lista_pos = []
+              for item in dados:
+                posicao = dados[f'{item}']
+                for elemento in posicao:
+                                if elemento == produto:
+                                  if item in lista_pos:
+                                    pass
+                                  else:
+                                    lista_pos.append(item)
+              if coletor:
+                origem = st.text_input(label='',placeholder='Insira a posição de Origem')
+                
+              else:
+                origem = st.selectbox(label='',placeholder='Insira a posição de Origem',options=lista_pos,index=None)
+              if origem:
+                colum2=origem [3:6]
+                Prat2=origem [0:2]
+                alt2 = origem [7:]
+                if len(colum2) != 3 or len(alt2)!= 2 or len(Prat2)!=2:
+                    st.error(f'A posição {origem} está incorreta. Insira-a novamente')
                     localizacao = None
-          if origem and produto and quantidade and final:
-              botao_transferir = st.button(f'Transferir {produto}')
-              if botao_transferir:
-                  if deposito_origem == 'Rec':
-                    requiscao = requests.get('https://bancodedadosroteirooficial-default-rtdb.firebaseio.com/.json')
-                    roteiro = requiscao.json()
-                    dados = roteiro['Depósito']
-                    quantidade_atual_rec = dados['Rec'][f'{produto}']['quantidade']
-                    deposito_ref = db.reference('Depósito')
-                    caminho_rec = f'Rec/{produto}/quantidade'
-                    nova_quantidade = quantidade_atual_rec-quantidade
-                    deposito_ref.child(caminho_rec).set(nova_quantidade)
-                    try: 
-                      dados['Rev'][f'{localizacao}'][f'{produto}']['quantidade']
-                      quantidade_atual_rec = dados['Rev'][f'{localizacao}'][f'{produto}']['quantidade']
-                      nova_quantidade_rev = quantidade_atual_rec + quantidade
-                      caminho_rev = f'Rev/{final}/{produto}'
-                      deposito_ref.child(caminho_rev).set({
-                      'quantidade':nova_quantidade_rev  # Exemplo de dado adicional
-                  })
-                      st.success(f'item {produto} transferido para a localização {final}')
-                    except:
-                      caminho_rev = f'Rev/{final}/{produto}'
-                      deposito_ref.child(caminho_rev).set({
-                      'quantidade':quantidade  # Exemplo de dado adicional
-                  })
-                      st.success(f'item {produto} transferido para a localização {final}')
-                  elif deposito_origem == 'Rev':
-                    requiscao = requests.get('https://bancodedadosroteirooficial-default-rtdb.firebaseio.com/.json')
-                    roteiro = requiscao.json()
-                    dados = roteiro['Depósito']
-                    quantidade_atual_rev_origem = dados['Rev'][f'{origem}'][f'{produto}']['quantidade']
-                    nova_quantidade_rev_origem =  quantidade_atual_rev_origem-quantidade
-                    deposito_ref = db.reference('Depósito')
-                    caminho_rev_origem = f'Rev/{origem}/{produto}/quantidade'
-                    deposito_ref.child(caminho_rev_origem).set(nova_quantidade_rev_origem)
-                    quantidade_atual_rev_final = dados['Rev'][f'{final}'][f'{produto}']['quantidade']
-                    nova_quantidade_rev_final =  quantidade_atual_rev_final+quantidade
-                    caminho_rev_final = f'Rev/{final}/{produto}/quantidade'
-                    deposito_ref.child(caminho_rev_final).set(nova_quantidade_rev_final)
-                    st.success(f'Produto {produto} transferido com sucesso')
-                    
-          else:
-                st.error('Ainda há campos a serem preenchidos')
-      else:
-        st.error(f'Item {produto} não está cadastrado')
+            quantidade = st.number_input(label='',placeholder='Insira a quantidade',value=None)
+            final = st.text_input(label='',placeholder='Insira a posição Final')
+            if final:
+                colum=final[3:6]
+                Prat=final[0:2]
+                alt = final[7:]
+                if len(colum) != 3 or len(alt)!= 2 or len(Prat)!=2:
+                      st.error(f'A posição {final} está incorreta. Insira-a novamente')
+                      localizacao = None
+            if origem and produto and quantidade and final:
+                botao_transferir = st.button(f'Transferir {produto}')
+                if botao_transferir:
+                    if deposito_origem == 'Rec':
+                      requiscao = requests.get('https://bancodedadosroteirooficial-default-rtdb.firebaseio.com/.json')
+                      roteiro = requiscao.json()
+                      dados = roteiro['Depósito']
+                      quantidade_atual_rec = dados['Rec'][f'{produto}']['quantidade']
+                      deposito_ref = db.reference('Depósito')
+                      caminho_rec = f'Rec/{produto}/quantidade'
+                      nova_quantidade = quantidade_atual_rec-quantidade
+                      deposito_ref.child(caminho_rec).set(nova_quantidade)
+                      try: 
+                        dados['Rev'][f'{localizacao}'][f'{produto}']['quantidade']
+                        quantidade_atual_rec = dados['Rev'][f'{localizacao}'][f'{produto}']['quantidade']
+                        nova_quantidade_rev = quantidade_atual_rec + quantidade
+                        caminho_rev = f'Rev/{final}/{produto}'
+                        deposito_ref.child(caminho_rev).set({
+                        'quantidade':nova_quantidade_rev  # Exemplo de dado adicional
+                    })
+                        st.success(f'item {produto} transferido para a localização {final}')
+                      except:
+                        caminho_rev = f'Rev/{final}/{produto}'
+                        deposito_ref.child(caminho_rev).set({
+                        'quantidade':quantidade  # Exemplo de dado adicional
+                    })
+                        st.success(f'item {produto} transferido para a localização {final}')
+                    elif deposito_origem == 'Rev':
+                      requiscao = requests.get('https://bancodedadosroteirooficial-default-rtdb.firebaseio.com/.json')
+                      roteiro = requiscao.json()
+                      dados = roteiro['Depósito']
+                      quantidade_atual_rev_origem = dados['Rev'][f'{origem}'][f'{produto}']['quantidade']
+                      nova_quantidade_rev_origem =  quantidade_atual_rev_origem-quantidade
+                      deposito_ref = db.reference('Depósito')
+                      caminho_rev_origem = f'Rev/{origem}/{produto}/quantidade'
+                      deposito_ref.child(caminho_rev_origem).set(nova_quantidade_rev_origem)
+                      quantidade_atual_rev_final = dados['Rev'][f'{final}'][f'{produto}']['quantidade']
+                      nova_quantidade_rev_final =  quantidade_atual_rev_final+quantidade
+                      caminho_rev_final = f'Rev/{final}/{produto}/quantidade'
+                      deposito_ref.child(caminho_rev_final).set(nova_quantidade_rev_final)
+                      st.success(f'Produto {produto} transferido com sucesso')
+                      
+            else:
+                  st.error('Ainda há campos a serem preenchidos')
+        else:
+          st.error(f'Item {produto} não está cadastrado')
   with col6:
             with st.popover('⚙️'):
                                   deposito_ref = db.reference('Depósito')
