6382fe26a2f4a546eec44df25b50e5c40a039878
juanpythonvolei <juanzsalca@outlook.com>
2024-08-28 22:53:09 -0300

    Update Tranferência.py

--- Estoque/pages/Tranferência.py
+++ Estoque/pages/Tranferência.py
@@ -83,8 +83,6 @@ if 'Depósito' in roteiro:
                   if coletor:
                     if deposito_origem == 'Rec' or deposito_origem == 'Dev'or deposito_origem == 'Ele':
                       origem = st.text_input(label='',placeholder='Insira a posição de Origem',value=deposito_origem)
-                    else:
-                      origem = st.text_input(label='',placeholder='Insira a posição de Origem')
                   else:
                     try:
                       origem = st.selectbox(label='',placeholder='Insira a posição de Origem',options=lista_pos,index=None,value=deposito_origem)
