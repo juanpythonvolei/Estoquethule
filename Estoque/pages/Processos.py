aa94bdb24df26f516a20b6b3b0b3bcf7f762fed4
juanpythonvolei <juanzsalca@outlook.com>
2024-09-03 23:51:27 -0300

    Update Processos.py

--- Estoque/pages/Processos.py
+++ Estoque/pages/Processos.py
@@ -167,7 +167,7 @@ with tab2:
         
         st.divider()   
   if len(lista_conferencia) == contagem:
-                caminho_faturamento = f'{selecao_datas}/{selecao_processos}'
+                caminho_faturamento = f'{selecao_datas}/{selecao_processos}/status'
                 ref_faturamento.child(caminho_faturamento).set('concluido')
                 st.success('Processo Concluido')   
           
