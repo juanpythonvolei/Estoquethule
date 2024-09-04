96cb8861289bc1f149894b9e35b4c47b96711b58
juanpythonvolei <juanzsalca@outlook.com>
2024-09-03 22:44:40 -0300

    Update Processos.py

--- Estoque/pages/Processos.py
+++ Estoque/pages/Processos.py
@@ -107,7 +107,7 @@ with tab2:
   dia = str(selecao_datas)[8:] 
   selecao_datas = f'{dia}-{mes}-{ano}'              
   if selecao_datas:  
-        for x in dados:
+        for x in dadose:
                 if x == selecao_datas:
                     notas = dados[f'{x}']
                     st.write(item)
