895d65379a1ec88a3061938431182fb6eb052c72
juanpythonvolei <juanzsalca@outlook.com>
2024-09-04 08:01:34 -0300

    Update Processos.py

--- Estoque/pages/Processos.py
+++ Estoque/pages/Processos.py
@@ -144,7 +144,7 @@ with tab2:
   lista_conclusao = []
   contagem = 0
   if 'concluido' not in lista_dicionarios:
-   for item in lista_dicionarios:  
+    for item in lista_dicionarios:  
         
         lista_conferencia = []
         qtd = int(item['quantidade'][0])
@@ -171,13 +171,13 @@ with tab2:
                     st.metric(f'Quantidade restante',value=len(lista_conferencia))
         
         st.divider()   
-   if len(lista_conferencia) == contagem:
+    if len(lista_conferencia) == contagem:
                 caminho_faturamento = f'{selecao_datas}/{selecao_processos}/status'
                 ref_faturamento.child(caminho_faturamento).set('concluido')
                 st.success('Processo Concluido')  
- else:
-  st.error('mercado já concluído')
-          
+  else:
+   st.error('mercado já concluído')
+           
             
                    
                 
