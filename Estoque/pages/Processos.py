4be8c58b00f07633f6929729e997274ec58172fb
juanpythonvolei <juanzsalca@outlook.com>
2024-09-04 21:44:20 -0300

    Update Processos.py

--- Estoque/pages/Processos.py
+++ Estoque/pages/Processos.py
@@ -257,8 +257,23 @@ with tab2:
    else:
     st.error('mercado já concluído')
 with tab3:
+  lista_processos = []
   ref_separacao = ('Separacao')
-            
+  selecao_datas=st.date_input(label='selecione uma data')
+  ano = str(selecao_datas)[0:4]
+  mes = str(selecao_datas)[5:7]
+  dia = str(selecao_datas)[8:] 
+  selecao_datas = f'{dia}-{mes}-{ano}'              
+  if selecao_datas:  
+    for a in dados:
+                if a == selecao_datas:
+                          infos = dados[f'{a}']
+                          for processo in infos:
+                            if processo in lista_processos:
+                              pass
+                            else:
+                              lista_processos.append(processo)
+  selecao_processos = st.selectbox(label='',placeholder='Selecione um Processo',index=None,options=lista_processos)             
                    
                 
         
