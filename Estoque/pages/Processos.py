62491ca76131731c0d599690f1e8de8f868c3fd5
juanpythonvolei <juanzsalca@outlook.com>
2024-09-05 07:23:30 -0300

    Update Processos.py

--- Estoque/pages/Processos.py
+++ Estoque/pages/Processos.py
@@ -174,12 +174,12 @@ with tab2:
      lista_ean.append(comparacao)
   with col7:
    selecao_datas=st.date_input(label='selecione uma data')
-  ano = str(selecao_datas)[0:4]
-  mes = str(selecao_datas)[5:7]
-  dia = str(selecao_datas)[8:] 
+   ano = str(selecao_datas)[0:4]
+   mes = str(selecao_datas)[5:7]
+   dia = str(selecao_datas)[8:] 
   
-  selecao_datas = f'{dia}-{mes}-{ano}'              
-  if selecao_datas:  
+   selecao_datas = f'{dia}-{mes}-{ano}'              
+   if selecao_datas:  
     for a in dados:
                 if a == selecao_datas:
                           infos = dados[f'{a}']
