a6072cb4482b7a0dce90c4617b41954d6ab5af91
juanpythonvolei <juanzsalca@outlook.com>
2024-09-06 18:25:42 -0300

    Update Processos.py

--- Estoque/pages/Processos.py
+++ Estoque/pages/Processos.py
@@ -268,7 +268,7 @@ with tab2:
                     st.metric(f'Quantidade restante',value=len(lista_conferencia))
         
         st.divider()   
-    if contagem == len(lista_dicionarios):
+   if contagem == len(lista_dicionarios):
                 caminho_faturamento = f'{selecao_datas}/{selecao_processos}/status'
                 ref_faturamento.child(caminho_faturamento).set('concluido')
                 caminho_faturamento = f'{selecao_datas}/{selecao_processos}/separacao'
