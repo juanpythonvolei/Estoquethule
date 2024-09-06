bf9f366c5dc753e29e915e70ad8d1405f467ce2e
juanpythonvolei <juanzsalca@outlook.com>
2024-09-06 17:47:46 -0300

    Update Processos.py

--- Estoque/pages/Processos.py
+++ Estoque/pages/Processos.py
@@ -283,7 +283,6 @@ with tab3:
                               notas = infos[f'{processo}']
                               
                               for espec in notas:
-                                  st.write(espec)
                                   if espec != 'status' and espec!='separacao':
                                     numero_nota = notas[f'{espec}']['nota']
                                     cliente = notas[f'{espec}']['cliente']
