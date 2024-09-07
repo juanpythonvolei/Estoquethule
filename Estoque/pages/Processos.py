82e03d08818fc99b45e9b3a073b949213a6ee9e1
juanpythonvolei <juanzsalca@outlook.com>
2024-09-07 11:41:31 -0300

    Update Processos.py

--- Estoque/pages/Processos.py
+++ Estoque/pages/Processos.py
@@ -234,7 +234,7 @@ with tab2:
   if selecao_datas and selecao_processos:       
     i = 0 
     
-    caminho_mercado = f'{selecao_datas}/{ selecao_processos}'
+    caminho_mercado = f'{selecao_datas}'
     try: 
       if 'já coletadoSS' in lista_dicionarios:
         st.info('Mercado já concluido')
@@ -250,7 +250,7 @@ with tab2:
             i += 1 
             if str(acao) == str(item['produtos']):
                 st.write('Feito')
-                dict_mercado = {'ean_volume':f'{random.randint(0,10000)}','itens':item['produtos'],'nota':item['numero_nota'],'posicao':item['posi'],'concluido':'nao'}
+                dict_mercado = {'processo':selecao_processos,'ean_volume':{random.randint(0,10000)},'itens':item['produtos'],'nota':item['numero_nota'],'posicao':item['posi'],'concluido':'nao','quantidade':item['quantidade']}
                 ref_mercado.child(caminho_mercado).set(dict_mercado)
                 st.success('Mercado registrado')
             st.divider() 
