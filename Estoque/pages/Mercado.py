01278d1df6d930c8939ce7d14c857e5e37b49ca3
juanpythonvolei <juanzsalca@outlook.com>
2024-09-08 21:48:22 -0300

    Update Mercado.py

--- Estoque/pages/Mercado.py
+++ Estoque/pages/Mercado.py
@@ -1,4 +1,34 @@
-    col7,col8,col9 = st.columns(3)
+import streamlit as st
+import firebase_admin
+from firebase_admin import credentials, firestore,db
+import requests
+import os
+import requests
+import random
+import xmltodict
+from datetime import datetime
+image = st.image('https://www.logolynx.com/images/logolynx/fe/fe346f78d111e1d702b44186af59b568.jpeg')
+lista_numero_processo = []
+requiscao = requests.get('https://bancodedadosroteirooficial-default-rtdb.firebaseio.com/.json')
+roteiro = requiscao.json()
+ref_faturamento = db.reference('Faturamento')
+ref_mercado = db.reference('mercado')  
+data_hora_atual = datetime.now()
+data_atual = data_hora_atual.strftime("%d-%m-%Y")
+caminho_faturamento = f'{data_atual}'
+#ref_faturamento.child(caminho_faturamento).set('a')
+dados = roteiro['Faturamento']
+dados2 = roteiro['Depósito']['Rev'] 
+try:
+  dados4 = roteiro['mercado']
+except:
+  dados4 = None
+try:
+  dados5 = roteiro['separacao']
+except:
+  dados5 = None
+
+col7,col8,col9 = st.columns(3)
     lista_ean = []
     lista_processos = []
     lista_datas =[]
