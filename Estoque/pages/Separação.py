5730b73b0dc14aa3b15040a2ed60af62e71762b5
juanpythonvolei <juanzsalca@outlook.com>
2024-09-08 21:22:17 -0300

    Update Separação.py

--- Estoque/pages/Separação.py
+++ Estoque/pages/Separação.py
@@ -1,3 +1,28 @@
+
+import streamlit as st
+import firebase_admin
+from firebase_admin import credentials, firestore,db
+import requests
+import os
+import requests
+import random
+import xmltodict
+from datetime import datetime
+image = st.image('https://www.logolynx.com/images/logolynx/fe/fe346f78d111e1d702b44186af59b568.jpeg')
+lista_numero_processo = []
+requiscao = requests.get('https://bancodedadosroteirooficial-default-rtdb.firebaseio.com/.json')
+roteiro = requiscao.json()
+ref_faturamento = db.reference('Faturamento')
+ref_mercado = db.reference('mercado')  
+data_hora_atual = datetime.now()
+data_atual = data_hora_atual.strftime("%d-%m-%Y")
+caminho_faturamento = f'{data_atual}'
+#ref_faturamento.child(caminho_faturamento).set('a')
+dados = roteiro['Faturamento']
+dados2 = roteiro['Depósito']['Rev'] 
+
+
+
 ref_separacao = db.reference('separacao')
 col1,col2,col3 = st.columns(3)
 lista_separacao = []
