99f6daeef6e87031949f1703dc5e8d4482a9b026
juanpythonvolei <juanzsalca@outlook.com>
2024-09-08 23:25:11 -0300

    Update Separação.py

--- Estoque/pages/Separação.py
+++ Estoque/pages/Separação.py
@@ -86,14 +86,7 @@ if selecao_datas_separacao:
            for m in dados5:
              if m == selecao_datas_separacao:
                tranpostador_m = dados5[f'{m}']
-               
-               if 'ltda.' in tranpostador_m:
-                 tranpostador_m = str(tranpostador_m).replace('ltda.','ltda')
-                 tranpostador_m = str(tranpostador_m).upper()
-               else:
-                 tranpostador_m = str(tranpostador_m).upper()
-               if str(selecao_transp) == str(tranpostador_m):
-                 for nota_m in tranpostador_m:
+               for nota_m in tranpostador_m:
                    
                    caracs = tranpostador_m[f'{nota_m}']
                    st.write(caracs)
