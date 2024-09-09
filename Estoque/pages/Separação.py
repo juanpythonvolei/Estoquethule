34192ad87009b7d801348f9542e3ca6bcbcf1bf5
juanpythonvolei <juanzsalca@outlook.com>
2024-09-08 23:41:01 -0300

    Update Separação.py

--- Estoque/pages/Separação.py
+++ Estoque/pages/Separação.py
@@ -81,13 +81,7 @@ if selecao_datas_separacao:
   
       
       if selecao_transp:
-         if 'ltda.' in selecao_transp:
-                     selecao_transp = str(selecao_transp)[:5].replace('ltda.','ltda')
-                     selecao_transp = str(selecao_transp)[:5].upper()
-         else:
-                     selecao_transp = str(selecao_transp)[:5].upper()  
-         
-         for dict in lista_separacao:
+           selecao_transp = str(selecao_transp)[:5].upper()
            separacao_ativa = False
            for m in dados5:
              if m == selecao_datas_separacao:
