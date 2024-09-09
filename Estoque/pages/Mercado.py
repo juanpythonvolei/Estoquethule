4db63b42fcbe2974458bbb058aeeb82c493e7fe9
juanpythonvolei <juanzsalca@outlook.com>
2024-09-09 15:25:39 -0300

    Update Mercado.py

--- Estoque/pages/Mercado.py
+++ Estoque/pages/Mercado.py
@@ -93,7 +93,7 @@ if selecao_datas and selecao_processos:
                                     else:  
                                         lista_dicionarios.append(dicionario)   
                                   except:
-                                    st.write(espec['posicao'])
+                                    st.write(espec)
                                     try:
                                       numero_nota = notas[f'{espec}']['nota']
                                     except:
