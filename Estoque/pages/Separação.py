5b23c1b6d7ff85eecb866757ea08414ec080c186
juanpythonvolei <juanzsalca@outlook.com>
2024-09-08 23:21:09 -0300

    Update Separação.py

--- Estoque/pages/Separação.py
+++ Estoque/pages/Separação.py
@@ -89,6 +89,8 @@ if selecao_datas_separacao:
                if 'ltda.' in tranpostador_m:
                  tranpostador_m = str(tranpostador_m).replace('ltda.','ltda')
                  tranpostador_m = str(tranpostador_m).upper()
+               else:
+                 tranpostador_m = str(tranpostador_m).upper()
                if str(selecao_transp)[:5] == str(tranpostador_m)[:5]:
                  for nota_m in tranpostador_m:
                    caracs = tranpostador_m[f'{nota_m}']
