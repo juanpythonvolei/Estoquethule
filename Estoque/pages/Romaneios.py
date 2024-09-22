8d45d18aaf617527c5868b0acc2eb98d9f7cb219
juanpythonvolei <juanzsalca@outlook.com>
2024-09-22 12:11:43 -0300

    Update Romaneios.py

--- Estoque/pages/Romaneios.py
+++ Estoque/pages/Romaneios.py
@@ -25,7 +25,7 @@ lista_transps = []
 separacoes = roteiro['separacao']
 for item in separacoes:
       if item == data_romaneio:
-            lista_transps.append(str(separacoes[f'{item}'].keys()))
+            st.write(separacoes[f'{item}'])
 if data_romaneio:
   with col1:
     transp  = st.selectbox(label='',placeholder='Selecione uma transportadora',options=lista_transps)
