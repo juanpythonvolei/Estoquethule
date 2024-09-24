1ab04d262e6b7a32c626068ca5250cf2e59102b9
juanpythonvolei <juanzsalca@outlook.com>
2024-09-24 19:00:02 -0300

    Update Romaneios.py

--- Estoque/pages/Romaneios.py
+++ Estoque/pages/Romaneios.py
@@ -13,6 +13,7 @@ requiscao = requests.get('https://bancodedadosroteirooficial-default-rtdb.fireba
 roteiro = requiscao.json()
 ref_faturamento = db.reference('Faturamento')
 ref_mercado = db.reference('mercado')  
+ref_romaneio = db.reference('romaneios') 
 data_hora_atual = datetime.now()
 data_atual = data_hora_atual.strftime("%d-%m-%Y")
 data_romaneio = st.date_input(label='Selecione uma data',value=None)
@@ -35,7 +36,26 @@ for item in separacoes:
             lista_transps.append(nome)
 if data_romaneio:
   with col1:
-    transp  = st.selectbox(label='',placeholder='Selecione uma transportadora',options=lista_transps)
-    
+    transp  = st.selectbox(label='',placeholder='Selecione uma transportadora',options=lista_transps)      
+    if transp:  
+        with col2:
+              botao_romaneios = st.button('Criar Romaneio')
+              for data_separacao in separacoes:
+                    if data_separacao == data_romaneio:
+                          transps == separacoes[f'{data_separacao}']:
+                          for elemento in transps:
+                                if elemento == transp:
+                                      notas = transp[f'{elemento}']
+                                      for espec in notas:
+                                            volumes = notas[f'{espec}']['volumes']
+                                            st.write(volumes)
+                    
+              #if botao_romaneios:
+                  #try:
+                        #for item in roteiro['romaneios']:
+                              #if item == data_romaneio:
+                                    #romaneios = data_romaneio[f'{item}']
+                                    #for romaneio in romaneios:
+                                          #numero_romaneio = romaneios[f'{romaneio}']['numero']
   
         
