203f5f3cb9aeb023b1a569b511a22f6c63da3ab6
juanpythonvolei <juanzsalca@outlook.com>
2024-09-24 19:48:28 -0300

    Update Romaneios.py

--- Estoque/pages/Romaneios.py
+++ Estoque/pages/Romaneios.py
@@ -37,8 +37,8 @@ for item in separacoes:
             nome = nome.replace('(','')
             nome = nome.replace(')','')
             nome = nome.replace("'","")
-            st.write(nome)
-            lista_transps.append(nome)
+            for item in nome:
+                  lista_transps.append(item)
 if data_romaneio:
   with col1:
     transp  = st.selectbox(label='',placeholder='Selecione uma transportadora',options=lista_transps,index=None)      
